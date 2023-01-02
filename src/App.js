fad1ae65b1337a18a4e0ec7e72525d2616f35414
kellzbellz45 <kelly@kellyfaria.com>
2023-01-02 16:56:05 -0500

    Added axios integration to display weather of a city

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Weather from "./Weather";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello world</p>
+        <Weather city="Lisbon" />
         <a
+          aria-label="link to this heading"
+          aria-describedby="introduction"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noreferrer"
+          // eslint-disable-next-line
+          alt="This is the react logo in green"
+        ></a>
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Audio } from "react-loader-spinner";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} C`
+    );
+  }
+  let apiKey = "9b4aa1e9d9995bf996aaf55cc902b1e5";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
