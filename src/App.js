e987d12d1ef1e09c7b1ca7c3993f1649095cd2de
kellzbellz45 <kelly@kellyfaria.com>
2023-01-15 13:35:40 -0500

    added boostrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import Search from "./Search.js";
 
 export default function App() {
   return (
-    <div>
-      <Search />
-      <p>
-        This React app is
-        <a href="https://github.com/KF-ShecodesHW/weather-react">
-          {" "}
-          opensourced
-        </a>{" "}
-        and built by kelly faria.
-      </p>
+    <div className="App">
+      <div className="Container">
+        <Search />
+        <p>
+          This React app is
+          <a href="https://github.com/KF-ShecodesHW/weather-react">
+            {" "}
+            open sourced
+          </a>{" "}
+          and built by kelly faria.
+        </p>
+      </div>
     </div>
   );
 }
